c8686c051753e88e4837c2c55910df9059c6359f
AditiKulkarni1697 <aditisujlegaonkar@gmail.com>
2023-07-21 23:12:55 +0530

    client

--- Frontend/clientpage/client.js
+++ Frontend/clientpage/client.js
@@ -1,5 +1,6 @@
 const user_details = document.getElementsByClassName("user_details");
-
-fetch("")
+const user_id = 1;
+fetch(`https://jsonplaceholder.typicode.com/users/${user_id}`)
   .then((res) => res.json())
-  .then((data) => console.log(data));
+  .then((data) => console.log(data))
+  .catch((err) => console.log(err));
